9e39775aa5091ae212c622dc02dec6b0f4632bac
Edgar Vergara <edgarvm13@hotmail.com>
2025-03-06 16:16:33 +0100

    add DTOs for user registration and login, implement validation in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,21 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @MessagePattern('auth.register.user')
-  registerUser() {
-    return 'register user';
+  registerUser(@Payload() registerUserDto: RegisterUserDto) {
+    return registerUserDto;
   }
 
   @MessagePattern('auth.login.user')
-  loginUser() {
-    return 'login user';
+  loginUser(@Payload() loginUserDto: LoginUserDto) {
+    return loginUserDto;
   }
   @MessagePattern('auth.verify.user')
   verifyYser() {
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -12,6 +13,13 @@ async function bootstrap() {
         servers: envs.natsServers,
       },
     },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   await app.listen();
