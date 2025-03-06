13d50189f5ddc5598a6e3bdb4cb7a8511a48e4ff
Edgar Vergara <edgarvm13@hotmail.com>
2025-03-06 17:04:04 +0100

    refactor auth service to include user registration logic, update User model to include name and unique email, and add bcrypt for password hashing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,7 +10,7 @@ export class AuthController {
 
   @MessagePattern('auth.register.user')
   registerUser(@Payload() registerUserDto: RegisterUserDto) {
-    return registerUserDto;
+    return this.authService.registerUser(registerUserDto);
   }
 
   @MessagePattern('auth.login.user')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
+import { RegisterUserDto } from './dto/register-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -8,5 +11,48 @@ export class AuthService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
     this.logger.log('MongoDB connected');
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { name, email, password } = registerUserDto;
+
+    try {
+      const user = await this.user.findUnique({
+        where: { email },
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists',
+        });
+      }
+
+      const newUser = await this.user.create({
+        data: {
+          email,
+          password: bcrypt.hashSync(password, 10),
+          name,
+        },
+      });
+
+      const { password: _, ...rest } = newUser;
+
+      void _;
+
+      return { user: rest, token: 'abc' };
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new RpcException({
+          status: 400,
+          message: error.message,
+        });
+      }
+
+      throw new RpcException({
+        status: 500,
+        message: 'Unknown error in auth.service - Auth Microservice',
+      });
+    }
   }
 }
